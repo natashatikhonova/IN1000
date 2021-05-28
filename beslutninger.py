f23436ac2cedda73da0d2815186196acdf2c08ee
Natasha Tikhonova <64562118+natashatikhonova@users.noreply.github.com>
2021-05-28 21:53:33 +0200

    Delete beslutninger.py

--- beslutninger.py
+++
@@ -1,9 +0,0 @@
-#Programmet tar inn input fra bruker og bruker if-statement for å
-#gi et svar basert på brukers input
-brus=input("Kunne du tenke deg til en brus?")
-if brus=="ja":
-    print("Her har du en brus!")
-elif brus=="nei":
-    print("Den er grei.")
-else:
-    print("Det forstod jeg ikke helt.")
